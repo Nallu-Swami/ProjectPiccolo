3fb3f7d436c1dc9513e027f674aec60468802a17
NalluSwami <aringopalan13489@gmail.com>
2023-05-19 09:36:00 +0530

    Start

--- main.py
+++ main.py
@@ -1,11 +1,2 @@
-import cv2 as cv
-
-# Load an image
-img = cv.imread('download.jpg')
-
-# Display the image in a window named "image"
-cv.imshow('image', img)
-
-# Wait for a key event and close the window
-cv.waitKey(0)
-cv.destroyAllWindows()
+import tensorflow as tf
+import numpy as np
